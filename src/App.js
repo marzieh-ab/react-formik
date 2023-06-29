cfd81c707b5977ffc9a8ffde9af75871f5925ea9
marzieh abidi <marzieh_abidi@yahoo.com>
2023-06-29 16:37:58 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import SignUp from "./SignUp";
+import Registration from "./Components/Register";
+
+const App = () => {
+  // return <SignUp />;
+  return <Registration />;
+};
+
+export default App
